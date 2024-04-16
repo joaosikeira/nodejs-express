27611b4104d3cde3e160a07a0d0435e3a38a4252
VrSiqueira <joao.siqueira@vrsoft.com.br>
2024-04-16 11:40:45 -0300

    Criado conexão com o banco Postgres

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
 //console.log("Hello, CodeLab!")
 
 import express from 'express'
-const app = express()
-const port = 3000
+import { Client } from 'pg'
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!')
-})
+async function main() {
+  
+  const app = express()
+  const port = 3000
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  const client = new Client()
+  await client.connect()
+  
+  const res = await client.query('SELECT $1::text as message', ['DB Connection is OK'])
+  console.log(res.rows[0].message) // Hello world!
+  await client.end()
+
+  app.get('/', (req, res) => {
+    res.send('Hello, World!')
+  })
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+}
+
+main()
